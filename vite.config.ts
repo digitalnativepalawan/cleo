d1150def3138b6098ef3806d23470d347f131308
digitalnativepalawan <growpalawan@gmail.com>
2025-09-15 00:07:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cleo/',      // <-- IMPORTANT for GitHub Pages under /cleo/
   plugins: [react()],
-  base: '/cleo/',   // IMPORTANT: matches your repo name
 })
